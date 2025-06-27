cbeb9b1ca91083a2313742db3132547d6709032d
MJOStudios <154002916+mrjosephs1101@users.noreply.github.com>
2025-06-27 17:13:25 -0400

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,15 @@
+// App.tsx
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider } from "@/components/theme-provider";
+
 import Search from "@/pages/search";
 import Results from "@/pages/results";
 import NotFound from "@/pages/not-found";
+import WingManPage from "@/pages/wingman"; // 👈 Add this
 
 function Router() {
   return (
@@ -14,6 +17,7 @@ function Router() {
       <Route path="/" component={Search} />
       <Route path="/search" component={Search} />
       <Route path="/sq/:searchId" component={Results} />
+      <Route path="/wingman" component={WingManPage} /> {/* 👈 Add route */}
       <Route component={NotFound} />
     </Switch>
   );
